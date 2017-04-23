afa9a13fd6ec2d56229f43fbd1e7ae4b4224bbbf
Murat Jumashev <jumasheff@gmail.com>
2017-04-23 17:57:36 +0600

    Add firebase config

--- tools/config.js
+++ tools/config.js
@@ -9,8 +9,8 @@
  */
 
 module.exports = {
-  title: 'React Static Boilerplate',        // Your website title
-  url: 'https://rsb.kriasoft.com',          // Your website URL
-  project: 'react-static-boilerplate',      // Firebase project. See README.md -> How to Deploy
-  trackingID: 'UA-XXXXX-Y',                 // Google Analytics Site's ID
+  title: 'Firebase SPA',           // Your website title
+  url: 'https://rsb.kriasoft.com', // Your website URL
+  project: 'fir-spa-f5655',         // Firebase project. See README.md -> How to Deploy
+  trackingID: 'UA-XXXXX-Y',        // Google Analytics Site's ID
 };
